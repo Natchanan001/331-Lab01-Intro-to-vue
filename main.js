81124f45ab94993deee4acca2762d20976715ecd
Natchanan001 <alwaysforever1112@gmail.com>
2025-06-27 09:55:43 +0700

    4.3

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const { createApp, ref } = Vue
 
 createApp({
   setup() {
-    const product = ref('Socks')
+    const product = ref('Boots')
+    const description = ref('Comfortable leather boots for hiking.')
+    const image = ref('assets/images/socks_green.jpg') 
+    const inStock = ref(true)
+
     return {
-      product
+      product,
+      description,
+      image,
+      inStock
     }
   }
 }).mount('#app')
