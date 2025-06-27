5cf8ad1f8f98e5f3d4269caaf73bdd2b60c0ef8e
Natchanan001 <alwaysforever1112@gmail.com>
2025-06-27 11:37:06 +0700

    8

--- main.js
+++ main.js
@@ -2,16 +2,46 @@ const { createApp, ref } = Vue
 
 createApp({
   setup() {
-    const product = ref('Boots')
-    const description = ref('Comfortable leather boots for hiking.')
-    const image = ref('assets/images/socks_green.jpg') 
+    const product = 'Boots'
+    const brand = ref('SE 331')
+    const image = ref('./assets/images/socks_green.jpg')
+    const onSale = true
     const inStock = ref(true)
+    const details = ['50% cotton', '30% wool', '20% polyester']
+    const sizes = ['S', 'M', 'L']
+    const cart = ref(0)
+    
+
+    const variants = ref([
+      { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+      { id: 2235, color: 'blue',  image: './assets/images/socks_blue.jpg' }
+    ])
+
+    function addToCart() {
+      cart.value++
+    }
+
+    function updateImage(variantImage) {
+      image.value = variantImage
+    }
+
+    function toggleStock() {
+      inStock.value = !inStock.value
+    }
 
     return {
       product,
-      description,
+      brand,
       image,
-      inStock
+      onSale,
+      inStock,
+      details,
+      sizes,
+      variants,
+      cart,
+      addToCart,
+      updateImage,
+      toggleStock
     }
   }
 }).mount('#app')
